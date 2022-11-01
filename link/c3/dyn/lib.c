f44b8045d9b171d86781fb4f5fbdbdb0ce7fd226
YoungTr <tangyan1004@gmail.com>
2022-11-01 11:30:57 +0800

    dyn

---
+++ link/c3/dyn/lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void foobar(int i)
+{
+	printf("Printing form lib.so %d\n", i);
+}
---
+++ link/c3/dyn/main.c
@@ -0,0 +1,9 @@
+int sum(int *a, int n);
+
+int array[] = {1, 2};
+
+int main()
+{
+	int val = sum(array, 2);
+	return val;
+}
